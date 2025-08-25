a47361adf03da982fbbec49380d71e9233376303
Eduardo Filho <eduardosmfilho01@gmail.com>
2025-08-25 07:28:52 +0100

    Funcionalities completed.

--- calcular_emprestimos/views.py
+++ calcular_emprestimos/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("test")
+    return render(request, "index.html")
+
+def resultado(request):
+    montante_final = None
+    juros = None
+    
+    if request.method == "POST":
+        
+        capital_inicial = float(request.POST.get('capital_inicial'))
+        taxa_de_juros_por_periodo = float(request.POST.get('taxa_de_juros_por_periodo'))
+        num_de_periodos = float(request.POST.get('num_de_periodos'))
+        
+        montante_final = capital_inicial * (1 + taxa_de_juros_por_periodo) ** num_de_periodos
+        juros = montante_final - capital_inicial
+    
+    return render(request, "resultado.html", {
+        "montante_final": montante_final,
+        "juros":juros
+    })
